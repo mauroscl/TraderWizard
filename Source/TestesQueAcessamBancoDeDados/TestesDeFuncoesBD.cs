ee8d280b17e0a3a6b4f08e7bf5650a0b8e40fb1a
mauroscl <mauroscl@gmail.com>
2013-07-06 16:49:44 -0300

    ajuste na função que desenha o IFR

---
+++ Source/TestesQueAcessamBancoDeDados/TestesDeFuncoesBD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class TestesDeFuncoesBD
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
