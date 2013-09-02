a821b1fbe4d74b16c58ed163409461b52724700f
mauroscl <mauroscl@gmail.com>
2013-09-01 23:14:43 -0300

    Inclusão do inicializador da aplicação

---
+++ Source/TraderWizard.Aplicacao.Inicializador/Inicializar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraderWizard.Aplicacao.Inicializador
+{
+    public class Inicializar
+    {
+        public void IniciaAplicacao()
+        {
+            ConfigurarNHibernate();
+        }
+
+        private void ConfigurarNHibernate()
+        {
+
+        }
+    }
+}
